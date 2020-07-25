28188f7983d8ce2f89e89c50821dd901d457c31c
Bryan D. Gonzalez <bryan.dujari@gmail.com>
2020-07-25 03:25:15 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,12 +1,18 @@
 var chocolateBars = ['snickers','hundred grand','kitkat','skittles']
 
+<<<<<<< HEAD
 function addElementToBeginningOfArray(chocolateBars, candyString){
   return [candyString, ...chocolateBars];
 }
+=======
+var addElementToBeginningOfArray = chocolateBars, fudge => fudge, ...chocolateBars
+
+>>>>>>> 5e89f29b50c944440d1495b9612a15b5fe1720bd
 function destructivelyAddElementToBeginningOfArray(chocolateBars, candyString){
   chocolateBars.unshift(candyString);
   return chocolateBars;
 }
+<<<<<<< HEAD
 function addElementToEndOfArray(chocolateBars, candyString){
   return [...chocolateBars, candyString];
 }
@@ -31,3 +37,15 @@ function destructivelyRemoveElementFromEndOfArray(chocolateBars){
 function removeElementFromEndOfArray(chocolateBars){
   return chocolateBars.slice(0, chocolateBars.length - 1);
 }
+=======
+
+var accessElementInArray = chocolateBars => chocolateBars[2]
+
+var destructivelyRemoveElementFromBeginningOfArray = chocolateBars => chocolateBars.shift()
+
+var removeElementFromBeginningOfArray = chocolateBars => chocolateBars.splice(1)
+
+var destructivelyRemoveElementFromEndOfArray = chocolateBars => chocolateBars.pop()
+
+var removeElementFromEndOfArray = chocolateBars => chocolateBars.splice(0, chocolateBars.length - 1)
+>>>>>>> 5e89f29b50c944440d1495b9612a15b5fe1720bd
